e571dc5d185deb3640b8df04ee507861faede2cc
DavidLapadula <lapadula.david@gmail.com>
2025-06-28 21:59:21 -0400

    [wee-2] add helper

---
+++ helpers.js
@@ -0,0 +1,9 @@
+function isPositiveNumber(value) {
+    const num = Number(value);
+    if (typeof num === 'number' && isFinite(num) && num > 0) {
+        return num;
+    }
+    return null;
+}
+
+module.exports = isPositiveNumber;
